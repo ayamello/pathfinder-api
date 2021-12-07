7cccd58f4d8aa59780594e8db3969b2f41e11e16
Carlos Lima <hiro1kz77@gmail.com>
2021-12-07 12:16:00 -0300

    feat: add activities table to the migration

--- app/configs/migrations.py
+++ app/configs/migrations.py
@@ -12,5 +12,6 @@ def init_app(app: Flask):
     from app.models.points_model import PointModel
     from app.models.points_paths_table import points_paths
     from app.models.adresses_model import AdressModel
+    from app.models.activities_model import ActivityModel
 
     
