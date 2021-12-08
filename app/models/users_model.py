c978ecee3d88767d8cdd545a28574e58fc2d179f
Carlos Lima <hiro1kz77@gmail.com>
2021-12-08 10:29:57 -0300

    feat: add new columns to users table and changed constraints of paths and activities

--- app/models/users_model.py
+++ app/models/users_model.py
@@ -7,17 +7,21 @@ from dataclasses import dataclass
 class UserModel(db.Model):
     id: int
     name: str
+    username: str
     email: str
     birthdate: str
+    url_image: str
     paths_list: list
 
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
+    username = db.Column(db.String(255), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     birthdate = db.Column(db.DateTime, nullable=False)
-    password_hash = db.Column(db.String)
+    url_image = db.Column(db.String)
+    password_hash = db.Column(db.String, nullable=False)
 
     paths_list = db.relationship('PathModel', backref=backref('user', uselist=False))
 
