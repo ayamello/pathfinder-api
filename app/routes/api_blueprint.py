c158b3398c4b5f383431c093cf716bd8043b3741
Miguel Maurer <miguel.maurer2001@gmail.com>
2021-12-08 17:47:36 -0300

    feat: signup and login

--- app/routes/api_blueprint.py
+++ app/routes/api_blueprint.py
@@ -1,6 +1,9 @@
 from flask import Blueprint
 from app.routes.activities_blueprint import bp as bp_activities
+from app.routes.user_blueprint import bp as bp_user
 
 bp = Blueprint("bp_api", __name__, url_prefix="/api")
 
 bp.register_blueprint(bp_activities)
+bp.register_blueprint(bp_user)
+
