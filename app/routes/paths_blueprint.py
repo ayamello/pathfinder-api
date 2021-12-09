ebadb7846b8f23924edf36d7968e204fce1d84ac
Ayana Mello <ayanamello27@gmail.com>
2021-12-08 21:17:43 -0300

    feat: develop create_path and delete_path

--- app/routes/paths_blueprint.py
+++ app/routes/paths_blueprint.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from app.controllers.paths_controller import create_path, delete_path
 
 
-bp = Blueprint('bp_path', __name__, url_prefix='/path')
+bp = Blueprint('bp_path', __name__, url_prefix='/paths')
 
 bp.post('')(create_path)
 bp.delete('/<int:id>')(delete_path)
