1b44b555e2bea4b67bc2f963b001813abff448c9
Carlos Lima <hiro1kz77@gmail.com>
2021-12-10 12:31:18 -0300

    refactor: changed user route session to login

--- app/routes/user_blueprint.py
+++ app/routes/user_blueprint.py
@@ -4,7 +4,7 @@ from app.controllers.user_controller import create_user, delete_user, get_all_us
 bp = Blueprint('bp', __name__)
 
 bp.post('/signup')(create_user)
-bp.post('/session')(login)
+bp.post('/login')(login)
 bp.get('/users')(get_all_users)
 bp.get('/user/<int:id>')(get_by_username)
 bp.patch('/user/<int:id>')(update_user)
