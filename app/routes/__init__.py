d72ba3378a90a40d852a7c58cb2f771a7c9eb1fb
Carlos Lima <hiro1kz77@gmail.com>
2021-12-10 00:34:30 -0300

    refactor: reestructured routes, api route deleted

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
-from app.routes.api_blueprint import bp as bp_api
+from app.routes.paths_blueprint import bp as bp_paths
+from app.routes.user_blueprint import bp as bp_user
 
 def init_app(app: Flask):
-    app.register_blueprint(bp_api)
+    app.register_blueprint(bp_paths)
+    app.register_blueprint(bp_user)
--- app/routes/points_blueprint.py
+++ app/routes/points_blueprint.py
@@ -1,10 +1,12 @@
 from flask import Blueprint
 from app.controllers.points_controller import create_point, update_point,delete_point, activities_by_point
+from app.routes.activities_blueprint import bp as bp_activities
 
 
 bp = Blueprint('bp_point', __name__, url_prefix='/points')
 
 bp.post('')(create_point)
-bp.get('/<int:id>')(activities_by_point)
+bp.get('/<int:id>/activities')(activities_by_point)
 bp.patch('/<int:id>')(update_point)
 bp.delete('/<int:id>')(delete_point)
+bp.register_blueprint(bp_activities)
