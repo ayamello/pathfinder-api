025152d750922ecdbb2b21dd3679b0d72901723c
Carlos Lima <hiro1kz77@gmail.com>
2021-12-10 00:54:46 -0300

    feat:add new routes

--- app/routes/paths_blueprint.py
+++ app/routes/paths_blueprint.py
@@ -1,13 +1,15 @@
 from flask import Blueprint
 from app.routes.points_blueprint import bp as bp_points
 from app.routes.subscribers_blueprint import bp as bp_subscribers
-from app.controllers.paths_controller import create_path, delete_path, read_all
+from app.controllers.paths_controller import create_path, delete_path, get_all_paths, get_paths_by_user_id, update_path
 
 
 bp = Blueprint('bp_path', __name__, url_prefix='/paths')
 
 bp.post('')(create_path)
 bp.delete('/<int:id>')(delete_path)
-bp.get('')(read_all)
+bp.patch('/<int:id>')(update_path)
+bp.get('')(get_all_paths)
+bp.get('/<int:id>')(get_paths_by_user_id)
 bp.register_blueprint(bp_points)
 bp.register_blueprint(bp_subscribers)
