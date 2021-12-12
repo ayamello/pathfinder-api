db609b6b959d453e8755089a07256f56905a7708
Ayana Mello <ayanamello27@gmail.com>
2021-12-12 18:51:10 -0300

    fix: fix configs send_emails

--- app/configs/smtp_gmail.py
+++ app/configs/smtp_gmail.py
@@ -8,17 +8,14 @@ env.read_env()
 mail = Mail()
 
 def init_app(app: Flask):
-    app.config.update(
-        MAIL_SERVER='smtp@gmail.com',
-        MAIL_PORT=465,
-        MAIL_USE_TSL=True,
-        MAIL_USERNAME=env('USER_EMAIL'),
-        MAIL_PASSWORD=env('PASSWORD_EMAIL')
-    )
+    app.config['MAIL_SERVER']='smtp.gmail.com'
+    app.config['MAIL_PORT']=465
+    app.config['MAIL_USE_SSL']=True
+    app.config['MAIL_USERNAME']=env('USER_EMAIL')
+    app.config['MAIL_PASSWORD']=env('PASSWORD_EMAIL')
+    app.config['MAIL_DEFAULT_SENDER']=env('USER_EMAIL')
 
     mail.init_app(app)
-
-    return mail
 
 
 def send_email():
