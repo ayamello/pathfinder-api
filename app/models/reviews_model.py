c223f8418e5b92ada22683a5222ed7e9341c65c5
Ayana Mello <ayanamello27@gmail.com>
2021-12-15 21:23:12 -0300

    fix: standardize routes

--- app/models/reviews_model.py
+++ app/models/reviews_model.py
@@ -7,12 +7,8 @@ from app.models.activities_model import ActivityModel
 
 @dataclass
 class ReviewModel(db.Model):
-<<<<<<< HEAD
     id: int
-    name: str
-=======
     username: str
->>>>>>> develop
     review: str
     created_at: str
     updated_at: str
@@ -28,8 +24,8 @@ class ReviewModel(db.Model):
       db.ForeignKey('activities.id'),
       nullable=False,
     )
-    created_at = db.Column(db.DateTime(timezone=True), default=datetime.now(timezone.utc))
-    updated_at = db.Column(db.DateTime(timezone=True), default=datetime.now(timezone.utc))
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    updated_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
 
     @staticmethod
     def validate(**kwargs):
