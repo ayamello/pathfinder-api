7cf7107301ba8edc5d20b0ccb69293f9cd46ebd0
Carlos Lima <hiro1kz77@gmail.com>
2021-12-16 23:56:08 -0300

    fix: fixing comments

--- app/controllers/users_controller.py
+++ app/controllers/users_controller.py
@@ -36,7 +36,7 @@ def create_user():
     try:
         data = request.get_json()
 
-        # send_email(**data)
+        send_email(**data)
 
         validated_data = UserModel.validate(**data)
         validated_data['birthdate'] = convert_date(validated_data['birthdate'])
@@ -86,8 +86,8 @@ def login():
         found_user.confirm_email = True
         current_app.db.session.commit()
 
-    # if found_user.confirm_email == False:
-    #     return {'error': 'Please activate your account'}, 409
+    if found_user.confirm_email == False:
+        return {'error': 'Please activate your account'}, 409
     
 
     if found_user.verify_password(data['password']):
